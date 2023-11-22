056e75d2bcf5da0bb61cce261f0d4a5ecdb8e575
Abhisekh Mukherjee <abhisekhmukherjee101@gmail.com>
2023-11-23 01:32:25 +0530

    10-remove-duplicate done - pretty simple stuff

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,10 @@
-function removeDuplicates() {}
+function removeDuplicates(duplicatedString) {
+  const uniqueString = new Set();
+  for (let i = 0; i < duplicatedString.length; i++) {
+    uniqueString.add(duplicatedString[i]);
+  }
+  //spreading the set value in a Array
+  return [...uniqueString];
+}
 
 module.exports = removeDuplicates;
