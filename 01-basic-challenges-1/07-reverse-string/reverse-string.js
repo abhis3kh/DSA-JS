da80c8d419716157ee469731d6cd642c58764c3e
Abhisekh Mukherjee <abhisekhmukherjee101@gmail.com>
2023-11-23 01:14:11 +0530

    08 Palindrome solved using two pointer using regex

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,10 @@
-function reverseString() {}
+function reverseString(reverseableString) {
+  // 1. Loop through the string and start the loop from end - starts appending the data
+  let reversedString = '';
+  for (let i = reverseableString.length - 1; i >= 0; i--) {
+    reversedString += reverseableString[i];
+  }
+  return reversedString;
+}
 
 module.exports = reverseString;
--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,20 @@
-function isPalindrome() {}
+function isPalindrome(palindromeString) {
+  //removing special and spaces from the string
+  let inputString = palindromeString.replace(/[^a-z0-9]/gi, '');
+  //
+  let startIndex = 0,
+    endIndex = inputString.length - 1;
+  while (startIndex !== endIndex) {
+    // console.log(`${inputString[startIndex]} : ${inputString[endIndex]}`);
+    if (
+      inputString[startIndex].toLowerCase() !==
+      inputString[endIndex].toLowerCase()
+    )
+      return false;
 
+    startIndex++;
+    endIndex--;
+  }
+  return true;
+}
 module.exports = isPalindrome;
