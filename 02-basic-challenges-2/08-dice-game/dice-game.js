5d9971eb26127235ddf96e8e5d8d3886186ff392
Abhisekh Mukherjee <abhisekhmukherjee101@gmail.com>
2023-11-26 03:09:49 +0530

    dice game - little trciky - should read details intensively before diving in

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,21 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numberOfSimulation) {
+  const overAllResult = [];
 
+  for (let i = 0; i < numberOfSimulation; i++) {
+    const diceRoll1 = Math.round(Math.random() * 5) + 1;
+    const diceRoll2 = Math.round(Math.random() * 5) + 1;
+    let result = '';
+    const sum = diceRoll1 + diceRoll2;
+    if (sum === 7 || sum === 11) result = 'win';
+    else if (sum === 2 || sum === 3 || sum === 12) result = 'lose';
+    else result = 'roll again';
+    overAllResult.push({
+      dice1: diceRoll1,
+      dice2: diceRoll2,
+      sum,
+      result,
+    });
+  }
+  return overAllResult;
+}
 module.exports = diceGameSimulation;
