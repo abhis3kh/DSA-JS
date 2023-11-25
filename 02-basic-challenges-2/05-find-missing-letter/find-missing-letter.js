bf8b940ebfa36607aa0033d6c58adab9faf7ed6d
Abhisekh Mukherjee <abhisekhmukherjee101@gmail.com>
2023-11-26 01:57:40 +0530

    solved missing letter using Unicode - by my own :)

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,13 @@
-function findMissingLetter() {}
+function findMissingLetter(lettersArray) {
+  //1. Take the first character unicode
+  //2. Take the last character unicode
+  //3. Loop each time and starts  adding 1 after initial item to check if that item exists or not
+  const firstUniCode = lettersArray[0].charCodeAt(0);
+  for (let i = 1; i < lettersArray.length; i++) {
+    if (firstUniCode + i != lettersArray[i].charCodeAt(0))
+      return String.fromCharCode(firstUniCode + i);
+  }
+  return '';
+}
 
 module.exports = findMissingLetter;
