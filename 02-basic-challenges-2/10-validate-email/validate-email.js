0831ffe33c722922219434b5cf97e1771463a766
Abhisekh Mukherjee <abhisekhmukherjee101@gmail.com>
2023-11-26 20:39:27 +0530

    Validate email & format phone number - done

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,17 @@
-function validateEmail() {}
-
+function validateEmail(inputString) {
+  let isEmail = false;
+  //1. First check if @ exists
+  //2. Once we got the @ - gets the string after @ to validate if it has . in it.
+  //3. Check if '.' exists in the sliced string after @ - If yes assign true or don't update
+  //4. return the value
+  let indexOfAtTheRateSymbol = inputString.indexOf('@');
+  if (indexOfAtTheRateSymbol > 0) {
+    //There is a @ in that string
+    const slicedTextAfterAtTheRate = inputString.slice(indexOfAtTheRateSymbol);
+    if (slicedTextAfterAtTheRate.indexOf('.') > 0) isEmail = true;
+  }
+  return isEmail;
+}
+console.log(validateEmail('john@gmail.com')); // true
+console.log(validateEmail('john@gmail')); // false
 module.exports = validateEmail;
