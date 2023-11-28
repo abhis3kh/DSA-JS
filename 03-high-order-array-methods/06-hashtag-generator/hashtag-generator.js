198099697a7369523e231af006f20424145efc60
Abhisekh Mukherjee <abhisekhmukherjee101@gmail.com>
2023-11-28 22:16:18 +0530

    Just started with HashTagGenerator Function

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,10 @@
-function generateHashtag() {}
-
+function generateHashtag(inputString) {
+  if (inputString.length < 1) return false;
+  const splittedArray = inputString.split(' ');
+  const hashTaggedString = splittedArray.reduce((word, appendedText) => {
+    return word + appendedText;
+  }, '#');
+  return hashTaggedString.length > 140 ? false : hashTaggedString;
+}
+console.log(generateHashtag('JavaScript is awesome'));
 module.exports = generateHashtag;
