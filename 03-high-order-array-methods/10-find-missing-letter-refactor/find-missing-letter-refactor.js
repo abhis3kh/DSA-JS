58e4df353269f58fa08345df6b609f2ad2dc0558
Abhisekh Mukherjee <abhisekhmukherjee101@gmail.com>
2023-12-01 01:59:11 +0530

    naive approach in missing letter refactor

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,12 @@
-function findMissingLetter() {}
-
+function findMissingLetter(letterArray) {
+  for (let index = 0; index < letterArray.length; index++) {
+    if (
+      letterArray[index].charCodeAt(0) + 1 !==
+      letterArray[index + 1].charCodeAt(0)
+    ) {
+      return String.fromCharCode(letterArray[index].charCodeAt(0) + 1);
+    }
+  }
+}
+// console.log(findMissingLetter(['a', 'b', 'c', 'd', 'f']));
 module.exports = findMissingLetter;
