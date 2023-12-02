251bca75c050a139e507475680b0bc2a8760148e
Abhisekh Mukherjee <abhisekhmukherjee101@gmail.com>
2023-12-02 23:31:52 +0530

    added README for High Order Array Methods

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,12 +1,27 @@
+// function findMissingLetter(letterArray) {
+//   //   for (let index = 0; index < letterArray.length; index++) {
+//   //     if (
+//   //       letterArray[index].charCodeAt(0) + 1 !==
+//   //       letterArray[index + 1].charCodeAt(0)
+//   //     ) {
+//   //       return String.fromCharCode(letterArray[index].charCodeAt(0) + 1);
+//   //     }
+//   //   }
+// })
+// module.exports = findMissingLetter;
+
 function findMissingLetter(letterArray) {
-  for (let index = 0; index < letterArray.length; index++) {
-    if (
-      letterArray[index].charCodeAt(0) + 1 !==
-      letterArray[index + 1].charCodeAt(0)
-    ) {
-      return String.fromCharCode(letterArray[index].charCodeAt(0) + 1);
+  const uniCodedLetters = letterArray.map((letter) => {
+    return letter.charCodeAt(0);
+  });
+  let missingValue = '';
+  missingValue = uniCodedLetters.reduce((result, currentCode) => {
+    if (currentCode + 1 !== result) {
+      missingValue = String.fromCharCode(currentCode + 1);
     }
-  }
+    return missingValue;
+  }, '');
+  return missingValue;
 }
-// console.log(findMissingLetter(['a', 'b', 'c', 'd', 'f']));
+
 module.exports = findMissingLetter;
